4c98ee8907afbf09f9ee46cdf04ecdafd8b55004
Brett Laptop <tri11paragon@tpgc.me>
2024-12-18 17:03:58 -0500

    button

--- app/app/src/main/java/com/mouseboy/finalproject/HomePage.java
+++ app/app/src/main/java/com/mouseboy/finalproject/HomePage.java
@@ -1,13 +1,16 @@
 package com.mouseboy.finalproject;
 
+import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
 
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.gson.GsonBuilder;
@@ -50,37 +53,38 @@ public class HomePage extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_home_page, container, false);
-        TextView textView = view.findViewById(R.id.textView);
-        view.findViewById(R.id.button).setOnClickListener(e -> {
-                Location current = LocationTracker.bestLocation();
-                ServerApi.AllWalkInfo walk = new ServerApi.AllWalkInfo("hewwow", new ServerApi.WalkInfo(), new ServerApi.WalkInstanceInfo[]{new ServerApi.WalkInstanceInfo()});
-                walk.walk.rating = 1.0;
-                walk.walk.start = new Date();
-                walk.walk.end = new Date();
-                walk.conditions[0].lat = current.getLatitude();
-                walk.conditions[0].lon = current.getLongitude();
-                walk.conditions[0].time = new Date();
 
-                WeatherApi.request(requireContext(), new WeatherApi.WeatherRequest(), conditions -> {
-                    walk.conditions[0].conditions = conditions.current;
-                    ServerApi.createWalk(requireContext(), walk, id -> {
-                        walk.walk.id = id;
-
-                        ServerApi.listWalkInfo(requireContext(), new ServerApi.WalkInfoId("hewwow", walk.walk.id), ok -> {
-                            textView.setText(new GsonBuilder().setPrettyPrinting().create().toJson(ok));
-                        }, Util::logThrowable);
-                    }, Util::logThrowable);
-                }, Util::logThrowable);
-
-            }
-        );
-
-        textView2 = view.findViewById(R.id.textView2);
-        view.findViewById(R.id.button2).setOnClickListener(e -> WeatherApi.request(requireContext(),
-            new WeatherApi.WeatherRequest(),
-            this::receiveReport,
-            Util::logThrowable
-        ));
+//        TextView textView = view.findViewById(R.id.textView);
+//        view.findViewById(R.id.button).setOnClickListener(e -> {
+//                Location current = LocationTracker.bestLocation();
+//                ServerApi.AllWalkInfo walk = new ServerApi.AllWalkInfo("hewwow", new ServerApi.WalkInfo(), new ServerApi.WalkInstanceInfo[]{new ServerApi.WalkInstanceInfo()});
+//                walk.walk.rating = 1.0;
+//                walk.walk.start = new Date();
+//                walk.walk.end = new Date();
+//                walk.conditions[0].lat = current.getLatitude();
+//                walk.conditions[0].lon = current.getLongitude();
+//                walk.conditions[0].time = new Date();
+//
+//                WeatherApi.request(requireContext(), new WeatherApi.WeatherRequest(), conditions -> {
+//                    walk.conditions[0].conditions = conditions.current;
+//                    ServerApi.createWalk(requireContext(), walk, id -> {
+//                        walk.walk.id = id;
+//
+//                        ServerApi.listWalkInfo(requireContext(), new ServerApi.WalkInfoId("hewwow", walk.walk.id), ok -> {
+//                            textView.setText(new GsonBuilder().setPrettyPrinting().create().toJson(ok));
+//                        }, Util::logThrowable);
+//                    }, Util::logThrowable);
+//                }, Util::logThrowable);
+//
+//            }
+//        );
+//
+//        textView2 = view.findViewById(R.id.textView2);
+//        view.findViewById(R.id.button2).setOnClickListener(e -> WeatherApi.request(requireContext(),
+//            new WeatherApi.WeatherRequest(),
+//            this::receiveReport,
+//            Util::logThrowable
+//        ));
         // Inflate the layout for this fragment
         return view;
     }
