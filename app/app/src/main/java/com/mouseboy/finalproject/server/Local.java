af842ad924107857973761d005a8a8cc7aea9394
ParkerTenBroeck <51721964+ParkerTenBroeck@users.noreply.github.com>
2024-12-18 17:05:58 -0500

    added the walking service

--- app/app/src/main/java/com/mouseboy/finalproject/server/Local.java
+++ app/app/src/main/java/com/mouseboy/finalproject/server/Local.java
@@ -1,17 +1,59 @@
 package com.mouseboy.finalproject.server;
 
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.mouseboy.finalproject.util.Util;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class Local {
     private static ServerApi.User currentUser = null;
-    private static final ArrayList<ServerApi.AllWalkInfo> walks = new ArrayList<>();
-    private static ServerApi.AllWalkInfo currentWalk;
+    private static Information state = new Information();
+
+    private static class Information{
+        final ArrayList<ServerApi.AllWalkInfo> walks = new ArrayList<>();
+        ServerApi.AllWalkInfo currentWalk;
+    }
 
 
     public static void addWalk(){
 
     }
 
+    private static File nya;
+    private static File getOut(Context context) {
+        if(nya == null){
+            String path = context.getFilesDir().getAbsolutePath();
+            path += File.separator;
+            path += "local.json";
+            nya = new File(path);
+        }
+        return nya;
+    }
+
+    public static void save(Context context){
+        try(FileOutputStream out = new FileOutputStream(getOut(context))) {
+            out.write(new Gson().toJson(state).getBytes(StandardCharsets.UTF_8));
+            out.flush();
+        } catch (IOException e) {
+            Util.logThrowable(e);
+        }
+    }
+
+    public static void load(Context context){
+        if (!getOut(context).exists()) return;
+        try(FileReader reader = new FileReader(getOut(context))) {
+            state = new Gson().fromJson(reader, Information.class);
+        } catch (Exception e) {
+            Util.logThrowable(e);
+        }
+    }
 
     public static boolean isUserLoggedIn(){
         return currentUser != null;
